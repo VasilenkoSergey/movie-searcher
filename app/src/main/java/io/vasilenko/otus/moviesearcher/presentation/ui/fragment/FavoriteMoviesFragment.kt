986f22ac705e6c6a3e5de20cb18ff59bf53fc5a9
Sergey Vasilenko <sergey@vasilenko.io>
2020-02-08 23:19:37 +0300

    Migrate to fragments

--- app/src/main/java/io/vasilenko/otus/moviesearcher/presentation/ui/fragment/FavoriteMoviesFragment.kt
+++ app/src/main/java/io/vasilenko/otus/moviesearcher/presentation/ui/fragment/FavoriteMoviesFragment.kt
@@ -1,15 +1,31 @@
 package io.vasilenko.otus.moviesearcher.presentation.ui.fragment
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.ItemTouchHelper
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import io.vasilenko.otus.moviesearcher.MovieSearcherApp
 import io.vasilenko.otus.moviesearcher.R
 import io.vasilenko.otus.moviesearcher.presentation.model.MovieModel
+import io.vasilenko.otus.moviesearcher.presentation.navigation.MoviesRouter
+import io.vasilenko.otus.moviesearcher.presentation.presenter.FavoriteMoviesPresenter
+import io.vasilenko.otus.moviesearcher.presentation.ui.activity.MovieDetailActivity
+import io.vasilenko.otus.moviesearcher.presentation.ui.adapter.FavoriteMoviesAdapter
+import io.vasilenko.otus.moviesearcher.presentation.ui.decoration.MovieItemDecoration
 import io.vasilenko.otus.moviesearcher.presentation.view.FavoriteMoviesView
+import kotlinx.android.synthetic.main.activity_favorite_movies.*
+import kotlinx.android.synthetic.main.activity_movies.*
 
 class FavoriteMoviesFragment : Fragment(), FavoriteMoviesView {
+
+    lateinit var router: MoviesRouter
+    lateinit var presenter: FavoriteMoviesPresenter
+    private lateinit var favoriteMoviesAdapter: FavoriteMoviesAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -19,11 +35,68 @@ class FavoriteMoviesFragment : Fragment(), FavoriteMoviesView {
         return inflater.inflate(R.layout.fragment_favorite_movies, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        router = MovieSearcherApp.router
+        presenter = MovieSearcherApp.favoriteMoviesPresenter
+        presenter.attachView(this@FavoriteMoviesFragment)
+        setupViews()
+        getFavoriteMovies()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        presenter.detachView()
+    }
+
     override fun getFavoriteMovies() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        presenter.loadFavoriteMovies()
     }
 
     override fun showFavoriteMovies(movies: List<MovieModel>) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        if (movies.isEmpty()) showEmptyPlaceHolder() else favoriteMoviesAdapter.setMovies(movies)
+    }
+
+    private fun setupViews() {
+        activity?.moviesAppToolBarText?.text = getString(R.string.movies_favorite_toolbar_title)
+        favoriteMoviesAdapter = FavoriteMoviesAdapter { movie -> movieClickListener(movie) }
+        favoriteMoviesRv.layoutManager = LinearLayoutManager(requireContext())
+        val padding = resources.getDimensionPixelSize(R.dimen.default_padding)
+        favoriteMoviesRv.addItemDecoration(
+            MovieItemDecoration(
+                padding
+            )
+        )
+        favoriteMoviesRv.adapter = favoriteMoviesAdapter
+
+        val swipeCallback = object : ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT) {
+
+            override fun onMove(
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder,
+                target: RecyclerView.ViewHolder
+            ): Boolean {
+                return false
+            }
+
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                presenter.deleteFromFavorites(favoriteMoviesAdapter.getMovieAt(viewHolder.adapterPosition))
+                favoriteMoviesAdapter.removeMovie(viewHolder.adapterPosition)
+                favoriteMoviesAdapter.notifyItemRemoved(viewHolder.adapterPosition)
+                if (favoriteMoviesAdapter.itemCount == 0) showEmptyPlaceHolder()
+            }
+        }
+
+        ItemTouchHelper(swipeCallback).attachToRecyclerView(favoriteMoviesRv)
+    }
+
+    private fun movieClickListener(movie: MovieModel) {
+        router.onOpenActivity(Intent(requireContext(), MovieDetailActivity::class.java).apply {
+            putExtra("movie", movie)
+        })
+    }
+
+    private fun showEmptyPlaceHolder() {
+        favoriteMoviesEmptyPlaceholder.visibility = View.VISIBLE
     }
 }
--- app/src/main/java/io/vasilenko/otus/moviesearcher/presentation/ui/fragment/TopMoviesFragment.kt
+++ app/src/main/java/io/vasilenko/otus/moviesearcher/presentation/ui/fragment/TopMoviesFragment.kt
@@ -1,15 +1,31 @@
 package io.vasilenko.otus.moviesearcher.presentation.ui.fragment
 
+import android.content.Intent
+import android.content.res.Configuration
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
+import com.google.android.material.snackbar.Snackbar
+import io.vasilenko.otus.moviesearcher.MovieSearcherApp
 import io.vasilenko.otus.moviesearcher.R
 import io.vasilenko.otus.moviesearcher.presentation.model.MovieModel
+import io.vasilenko.otus.moviesearcher.presentation.navigation.MoviesRouter
+import io.vasilenko.otus.moviesearcher.presentation.presenter.TopMoviesPresenter
+import io.vasilenko.otus.moviesearcher.presentation.ui.activity.MovieDetailActivity
+import io.vasilenko.otus.moviesearcher.presentation.ui.adapter.TopMoviesAdapter
+import io.vasilenko.otus.moviesearcher.presentation.ui.decoration.MovieItemDecoration
 import io.vasilenko.otus.moviesearcher.presentation.view.TopMoviesView
+import kotlinx.android.synthetic.main.activity_movies.*
+import kotlinx.android.synthetic.main.activity_top_movies.*
 
 class TopMoviesFragment : Fragment(), TopMoviesView {
+
+    lateinit var router: MoviesRouter
+    lateinit var presenter: TopMoviesPresenter
+    private lateinit var topMoviesAdapter: TopMoviesAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -19,19 +35,77 @@ class TopMoviesFragment : Fragment(), TopMoviesView {
         return inflater.inflate(R.layout.fragment_top_movies, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        router = MovieSearcherApp.router
+        presenter = MovieSearcherApp.topMoviesPresenter
+        presenter.attachView(this@TopMoviesFragment)
+        setupViews()
+        getTopMovies()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        presenter.detachView()
+    }
+
     override fun getTopMovies() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        presenter.loadTopMovies()
     }
 
     override fun showTopMovies(movies: List<MovieModel>) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        topMoviesAdapter.setMovies(movies)
     }
 
     override fun showMessageOnSuccessfulAddingToFavorites() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        Snackbar.make(
+            activity?.moviesContainer as View,
+            getString(R.string.added_to_favorites),
+            Snackbar.LENGTH_SHORT
+        ).show()
     }
 
     override fun showMessageIfMovieExistInFavorites() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        Snackbar.make(
+            activity?.moviesContainer as View,
+            getString(R.string.already_favorite),
+            Snackbar.LENGTH_SHORT
+        ).show()
+    }
+
+    private fun setupViews() {
+        activity?.moviesAppToolBarText?.text = getString(R.string.movies_top_toolbar_title)
+        topMoviesAdapter = TopMoviesAdapter(
+            { movie -> movieOpenClickListener(movie) },
+            { movie -> movieAddToFavoriteClickListener(movie) }
+        )
+        topMoviesRv.layoutManager = when (resources.configuration.orientation) {
+            Configuration.ORIENTATION_PORTRAIT ->
+                GridLayoutManager(requireContext(), PORTRAIT_SPAN_COUNT)
+            else ->
+                GridLayoutManager(requireContext(), LANDSCAPE_SPAN_COUNT)
+        }
+        val padding = resources.getDimensionPixelSize(R.dimen.default_padding)
+        topMoviesRv.addItemDecoration(
+            MovieItemDecoration(
+                padding
+            )
+        )
+        topMoviesRv.adapter = topMoviesAdapter
+    }
+
+    private fun movieOpenClickListener(movie: MovieModel) {
+        router.onOpenActivity(Intent(requireContext(), MovieDetailActivity::class.java).apply {
+            putExtra("movie", movie)
+        })
+    }
+
+    private fun movieAddToFavoriteClickListener(movie: MovieModel) {
+        presenter.addMovieToFavorites(movie)
+    }
+
+    private companion object {
+        const val PORTRAIT_SPAN_COUNT = 2
+        const val LANDSCAPE_SPAN_COUNT = 3
     }
 }
