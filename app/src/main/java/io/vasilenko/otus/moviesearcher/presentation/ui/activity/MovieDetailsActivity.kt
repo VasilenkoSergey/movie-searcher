d1df0936be7cea33950516e01d934e7490913662
Sergey Vasilenko <sergey@vasilenko.io>
2020-02-09 01:25:48 +0300

    New movie details view with collapsing toolbar

--- app/src/main/java/io/vasilenko/otus/moviesearcher/presentation/ui/activity/MovieDetailsActivity.kt
+++ app/src/main/java/io/vasilenko/otus/moviesearcher/presentation/ui/activity/MovieDetailsActivity.kt
@@ -5,21 +5,20 @@ import androidx.appcompat.app.AppCompatActivity
 import io.vasilenko.otus.moviesearcher.R
 import io.vasilenko.otus.moviesearcher.presentation.model.MovieModel
 import io.vasilenko.otus.moviesearcher.presentation.view.MovieDetailsView
-import kotlinx.android.synthetic.main.activity_movie_details.*
+import kotlinx.android.synthetic.main.activity_movie_details_new.*
 
 class MovieDetailsActivity : AppCompatActivity(), MovieDetailsView {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_movie_details)
+        setContentView(R.layout.activity_movie_details_new)
+        detailsBackButton.setOnClickListener { finish() }
         showMovie()
     }
 
     override fun showMovie() {
         val movie = intent.getParcelableExtra<MovieModel>("movie")
-        movieTitle.text = movie?.title
         movieDescription.text = movie?.description
-        movieRating.text = movie?.rating
         movie?.imgId?.let { movieImg.setImageResource(it) }
     }
 }
