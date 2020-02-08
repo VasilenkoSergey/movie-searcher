6a8482fc2c0949c1d17282100f16654c3d3e97f0
Sergey Vasilenko <sergey@vasilenko.io>
2020-02-08 22:29:30 +0300

    Layout for main movies activity

--- app/src/main/java/io/vasilenko/otus/moviesearcher/presentation/ui/activity/MoviesActivity.kt
+++ app/src/main/java/io/vasilenko/otus/moviesearcher/presentation/ui/activity/MoviesActivity.kt
@@ -1,12 +1,19 @@
 package io.vasilenko.otus.moviesearcher.presentation.ui.activity
 
 import android.content.Intent
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import io.vasilenko.otus.moviesearcher.R
 import io.vasilenko.otus.moviesearcher.presentation.navigation.MoviesRouterHandler
 import io.vasilenko.otus.moviesearcher.presentation.view.MoviesView
 
 class MoviesActivity : AppCompatActivity(), MoviesView, MoviesRouterHandler {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_movies)
+    }
 
     override fun showTopMovies() {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
